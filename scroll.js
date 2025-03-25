b27771c4c528d5b780f6da667570d5f38b82b65c
Han <hanin.haydar@hotmail.com>
2025-03-25 19:03:17 +0000

    Added second 'Why Work With Us' section + WhatsApp link

---
+++ scroll.js
@@ -0,0 +1,15 @@
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1,
+});
+
+fadeIns.forEach(fade => {
+  observer.observe(fade);
+});
